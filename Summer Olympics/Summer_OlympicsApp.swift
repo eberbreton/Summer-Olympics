b0b52e270e210e88f741f2c1ec2f258e8bf7c07c
eberbreton <eberbreton200@gmail.com>
2025-05-19 11:09:45 -0700

    Initial Commit

---
+++ Summer Olympics/Summer_OlympicsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Summer_OlympicsApp.swift
+//  Summer Olympics
+//
+//  Created by Student1 on 5/19/25.
+//
+
+import SwiftUI
+
+@main
+struct Summer_OlympicsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
